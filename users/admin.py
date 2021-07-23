3d67c9bfa8e762d0a34a4f46d1a928d08f755138
Achelous1 <jsdesign1204@alignup.io>
2021-07-23 14:34:01 +0900

    Addtion to #3.6

--- users/admin.py
+++ users/admin.py
@@ -8,6 +8,7 @@ class CustomUserAdmin(UserAdmin):
 
     """Custom User Admin"""
 
+    # 장고 어드민 뷰에서 필드셋으로 폼필드를 나눌 수 있다
     fieldsets = UserAdmin.fieldsets + (
         (
             "Custom fieldset",
