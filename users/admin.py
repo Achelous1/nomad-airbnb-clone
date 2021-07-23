ff9f425efc02a9c1c53d57f065de83bbf4e829ad
Achelous1 <jsdesign1204@alignup.io>
2021-07-23 14:32:51 +0900

    Completd #3.6 UserAdmin + CustomAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,21 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """Custom User Admin"""
 
-    # admin site 리스트에 어떤 항목들을 나타낼지 결정한다
-    list_display = ("username", "gender", "language", "currency", "superhost")
-
-    # admin site에서 어떤 필터를 보여줄 지 결정한다
-    list_filter = ("language", "superhost")
-
-
-# 위 코드와 같음
-# class CustomUserAdmin(admin.ModelAdmin):
-#     pass
-#
-# admin.site.register(models.User, CustomUserAdmin)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom fieldset",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                ),
+            },
+        ),
+    )
