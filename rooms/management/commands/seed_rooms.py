717a80c241dbed8c09fdda21fee671105f26855b
Achelous1 <jsdesign1204@alignup.io>
2021-08-15 10:30:22 +0900

    Completed #9.6 seed_reviews

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -37,14 +37,35 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(1, 5),
             },
         )
+        # 생성 된 Room객체를 대상으로 객실 사진을 넣어주는 로직
         created_rooms = seeder.execute()
         created_clean = flatten(list(created_rooms.values()))
+        amenities = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        rules = room_models.HouseRule.objects.all()
         for pk in created_clean:
             room = room_models.Room.objects.get(pk=pk)
-            for i in range(3, random.randint(10, 17)):
+            for i in range(3, random.randint(10, 30)):
                 room_models.Photo.objects.create(
                     caption=seeder.faker.sentence(),
                     room=room,
                     file=f"room_photos/{random.randint(1,31)}.webp",  # 파일경로가 아닌 타깃 URL
                 )
+
+            # many to many 필드를 주입할 때 add함수를 사용한다
+            for a in amenities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.amenities.add(a)
+
+            for f in facilities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.facilities.add(f)
+
+            for r in rules:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.house_rules.add(r)
+
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
